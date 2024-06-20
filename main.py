b213081a2c84d81e6fdf5576390f17d137453f19
CesarHPMP <cesarmelo812@gmail.com>
2024-06-20 19:44:23 -0300

    Adicionando import's necessarios.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from keras_applications import vgg16
 from keras_applications.vgg16 import VGG16
+from keras_preprocessing.image import load_img
+from keras_applications.imagenet_utils import preprocess_input
+
+
 
 model = VGG16()
