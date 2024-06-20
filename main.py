feaf213af3fbda169aade96655deb7f5a60cab49
CesarHPMP <cesarmelo812@gmail.com>
2024-06-20 19:59:20 -0300

    adicionando data para o modelo.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from keras_applications import vgg16
 from keras_applications.vgg16 import VGG16
 from keras_preprocessing.image import load_img
+from keras_preprocessing import image
 from keras_applications.imagenet_utils import preprocess_input
+import numpy as np
 
+image = load_img('./data/teste.jpeg', target_size=(224, 224))
 
+image = np.array(image)
 
-model = VGG16()
+image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+
+image = preprocess_input(image)
+
+my_image = imread('./data/teste.jpeg')
