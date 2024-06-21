3c39c54a00618cbea3941c2391863bb0b48c8ff5
CesarHPMP <cesarmelo812@gmail.com>
2024-06-20 21:36:51 -0300

    Problemas a serem resolvidos.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from keras_applications import vgg16
-from keras_applications.vgg16 import VGG16
 from keras_preprocessing.image import load_img
 from keras_preprocessing import image
-from keras_applications.imagenet_utils import preprocess_input
+# AttributeError: 'NoneType' object has no attribute 'image_data_format'
+from keras_applications.resnet50 import decode_predictions, preprocess_input
 import numpy as np
+
 
 image = load_img('./data/teste.jpeg', target_size=(224, 224))
 
@@ -12,5 +12,3 @@ image = np.array(image)
 image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 
 image = preprocess_input(image)
-
-my_image = imread('./data/teste.jpeg')
